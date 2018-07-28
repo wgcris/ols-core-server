050666c026a26935fd9c2ef517584acec43a0810
Gao-li <lxagao@thoughtworks.com>
2018-07-28 23:26:44 +0800

    refactor unified exception handler

--- src/main/java/com/thoughtworks/nho/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/nho/exception/GlobalExceptionHandler.java
@@ -2,34 +2,28 @@ package com.thoughtworks.nho.exception;
 
 import com.thoughtworks.nho.util.ResponseInfo;
 import com.thoughtworks.nho.util.ResultCode;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public ResponseInfo handlerException(Exception e) {
 
-        //如果是自定义的异常，返回对应的错误信息
-        if (e instanceof InvalidCredentialException) {
-            e.printStackTrace();
-            InvalidCredentialException invalidCredentialException = (InvalidCredentialException) e;
-            return new ResponseInfo(ResultCode.INVALID_CREDENTIAL.getCode(), invalidCredentialException.getMessage());
-        } else if (e instanceof UserNotExistException) {
-            e.printStackTrace();
-            UserNotExistException userNotExistException = (UserNotExistException) e;
-            return new ResponseInfo(ResultCode.USER_NOT_EXISTED.getCode(), userNotExistException.getMessage());
-        } else if (e instanceof UserExistedException) {
-            e.printStackTrace();
-            UserExistedException userExistedException = (UserExistedException) e;
-            return new ResponseInfo(ResultCode.USER_EXISTED.getCode(), userExistedException.getMessage());
-        } else {
-            e.printStackTrace();
-            return new ResponseInfo(ResultCode.SYSTEM_EXCEPTION.getCode(),"system error!");
-        }
-
+    @ExceptionHandler({InvalidCredentialException.class, UserNotExistException.class, UserExistedException.class})
+    public ResponseInfo handlerCustomizeException(Exception e) {
+        log.debug(e.getMessage());
+        return new ResponseInfo(ResultCode.CUSTOMIZE_ERROR.getCode(), e.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseInfo handlerSystemException(Exception e) {
+        log.error(e.getMessage());
+        return new ResponseInfo(ResultCode.SYSTEM_EXCEPTION.getCode(), "system error");
+    }
+
+
 }
--- src/main/java/com/thoughtworks/nho/util/ResultCode.java
+++ src/main/java/com/thoughtworks/nho/util/ResultCode.java
@@ -1,11 +1,9 @@
 package com.thoughtworks.nho.util;
 
 public enum ResultCode {
-    USER_EXISTED("-1"),
     USER_NAME_ERROR("-1"),
     USER_PWD_ERROR("-1"),
-    USER_NOT_EXISTED("-1"),
-    INVALID_CREDENTIAL("-1"),
+    CUSTOMIZE_ERROR("-1"),
     SYSTEM_EXCEPTION("-1"),
     SUCCESS("0");
 
