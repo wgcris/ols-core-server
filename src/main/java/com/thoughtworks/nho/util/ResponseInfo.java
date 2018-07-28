92956779da33ebad9c94a3acc5c6969282cfdf9a
Gao-li <lxagao@thoughtworks.com>
2018-07-28 21:46:16 +0800

    ResponseInfo add Getter

--- src/main/java/com/thoughtworks/nho/util/ResponseInfo.java
+++ src/main/java/com/thoughtworks/nho/util/ResponseInfo.java
@@ -1,19 +1,22 @@
 package com.thoughtworks.nho.util;
 
+import lombok.Getter;
+
+@Getter
 public class ResponseInfo{
     private String code;
     private String message;
     private Object data;
-    public ResponseInfo(String code,String message){
+
+    public ResponseInfo(String code, String message) {
         this.code = code;
         this.message =message;
     }
 
-    public ResponseInfo(String code,Object data){
+    public ResponseInfo(String code, Object data) {
         this.code =code;
         this.data =data;
     }
-
 
 
 }
